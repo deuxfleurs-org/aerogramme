72b2b8ca348a1e8c42c074eea9105a30a4e06b24
Armaël Guéneau <armael.gueneau@ens-lyon.org>
2026-09-03 15:41:02 +0200

    imap: only disconnect on CONDSTORE-enabled sessions after MODSEQ invalidation

--- aero-proto/src/imap/mailbox_view.rs
+++ aero-proto/src/imap/mailbox_view.rs
@@ -129,7 +129,7 @@ impl MailboxView {
         if cur_state.uidvalidity != self.known_state.uidvalidity {
             return Err(SyncError::UidvalidityChanged(cur_state.uidvalidity).into());
         }
-        if cur_state.modseqvalidity != self.known_state.modseqvalidity {
+        if self.is_condstore && cur_state.modseqvalidity != self.known_state.modseqvalidity {
             return Err(SyncError::ModseqvalidityChanged(cur_state.modseqvalidity).into());
         }
         Ok(())
@@ -152,7 +152,7 @@ impl MailboxView {
         if cur_state.uidvalidity != self.known_state.uidvalidity {
             return Err(SyncError::UidvalidityChanged(cur_state.uidvalidity).into());
         }
-        if cur_state.modseqvalidity != self.known_state.modseqvalidity {
+        if self.is_condstore && cur_state.modseqvalidity != self.known_state.modseqvalidity {
             return Err(SyncError::ModseqvalidityChanged(cur_state.modseqvalidity).into());
         }
         Ok(())
